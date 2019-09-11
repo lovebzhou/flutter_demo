84bafb2e9a31c30acef85ad66e79062a29a4d354
lovebzhou <lovebzhou@gmail.com>
2019-09-11 17:01:37 +0800

    minor modify

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,21 @@ class AppPage extends StatefulWidget {
   State<StatefulWidget> createState() => _AppPageState();
 }
 
-class _AppPageState extends State<AppPage> {
+class _AppPageState extends State<AppPage> with WidgetsBindingObserver {
   Widget home = HBWelcomePage();
 
   @override
+  void dispose() {
+
+    WidgetsBinding.instance.removeObserver(this);
+    
+    super.dispose();
+  }
+  @override
   void initState() {
     super.initState();
+    
+    WidgetsBinding.instance.addObserver(this);
 
     eventBus.on<UserAuthEvent>().listen((e) {
       setState(() {
@@ -32,5 +41,31 @@ class _AppPageState extends State<AppPage> {
       theme: HBTheme.mainThemeData,
       home: home,
     );
+  }
+
+  /// 系统窗口相关改变回调，例如旋转
+  void didChangeMetrics() {
+    print('didChangeMetrics');
+  }
+
+  /// 文字系数变化
+  void didChangeTextScaleFactor() {
+    print('didChangeTextScaleFactor');
+  }
+
+  //本地化语言变化
+  void didChangeLocales(List<Locale> locale) {
+    print('didChangeLocales');
+  }
+
+  //生命周期变化
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('didChangeAppLifecycleState');
+  }
+
+  //低内存回调
+  void didHaveMemoryPressure() {
+    print('didHaveMemoryPressure');
+    PaintingBinding.instance.imageCache.clear();
   }
 }
