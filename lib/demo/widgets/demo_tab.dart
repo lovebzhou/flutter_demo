1a36783555dccc0aeb942129d3e9ee4a9e55b4c7
lovebzhou <lovebzhou@gmail.com>
2019-10-22 09:19:20 +0800

    improve tabbar demo

--- lib/demo/widgets/demo_tab.dart
+++ lib/demo/widgets/demo_tab.dart
@@ -8,7 +8,7 @@ class DemoTab extends StatefulWidget {
 }
 
 class _DemoTabState extends State<DemoTab> with SingleTickerProviderStateMixin {
-  final List<Map<String, dynamic>> tabDatas = [
+  final List<Map<String, dynamic>> _tabDatas = [
     {'name': 'tab1'},
     {'name': 'tab2'},
     {'name': 'tab3'},
@@ -18,14 +18,34 @@ class _DemoTabState extends State<DemoTab> with SingleTickerProviderStateMixin {
     {'name': 'tab7'},
   ];
 
+  List<Widget> _tabs;
+  List<Widget> _tabViews;
+
   TabController _tabController;
   @override
   void initState() {
     super.initState();
 
-    _tabController = TabController(length: tabDatas.length, vsync: this);
-    _tabController.animation.addListener((){
-      Log.d('animation.value = ${_tabController.animation.value}, index = ${_tabController.index}, prevIndex = ${_tabController.previousIndex}');
+    _tabController = TabController(length: _tabDatas.length, vsync: this);
+    _tabController.animation.addListener(() {
+      Log.d(
+          'animation.value = ${_tabController.animation.value}, index = ${_tabController.index}, prevIndex = ${_tabController.previousIndex}');
+    });
+
+    _tabs = [];
+    _tabViews = [];
+    _tabDatas.forEach((data) {
+      Tab tab = Tab(
+        child: Container(
+          child: Text(data['name']),
+        ),
+      );
+      _tabs.add(tab);
+
+      Widget child = Center(
+        child: Text(data['name']),
+      );
+      _tabViews.add(child);
     });
   }
 
@@ -47,62 +67,72 @@ class _DemoTabState extends State<DemoTab> with SingleTickerProviderStateMixin {
   }
 
   Widget _buildBody(BuildContext context) {
-    List<Widget> tabs = [];
-    List<Widget> children = [];
-    tabDatas.forEach((data) {
-      Tab tab = Tab(
-        child: Container(
-          child: Text(data['name']),
-        ),
-      );
-      tabs.add(tab);
-
-      Widget child = Center(
-        child: Text(data['name']),
-      );
-      children.add(child);
-    });
-
-    Widget tabBar = TabBar(
-      controller: _tabController,
-      isScrollable: true,
-      tabs: tabs,
-    );
-
-    Widget tabBarView = Expanded(
-      child: TabBarView(
-        controller: _tabController,
-        children: children,
-      ),
-    );
-
-    return Column(
+    Widget body = Column(
       children: <Widget>[
-        tabBar,
-        tabBarView,
+        _buildTabBar(context),
+        _buildTabBar(context),
         _buildFooter(context),
       ],
     );
+
+    body = SafeArea(child: body);
+
+    return body;
+  }
+
+  Widget _buildTabBar(BuildContext context) {
+    Widget tabBar = TabBar(
+      controller: _tabController,
+      isScrollable: true,
+      tabs: _tabs,
+    );
+    return tabBar;
+  }
+
+  Widget _buildTabBarView(BuildContext context) {
+    Widget tabBarView = Expanded(
+      child: TabBarView(
+        controller: _tabController,
+        children: _tabViews,
+      ),
+    );
+    return tabBarView;
   }
 
   Widget _buildFooter(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          RaisedButton(
-            child: Text('重置'),
-            onPressed: () {
-              Log.d('重置');
-            },
+          Expanded(
+            child: RaisedButton(
+              child: Text('重置'),
+              onPressed: () {
+                Log.d('重置');
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(6.0),
+                  bottomLeft: Radius.circular(6.0),
+                ),
+              ),
+            ),
           ),
-          RaisedButton(
-            child: Text('确定'),
-            onPressed: () {
-              Log.d('确定');
-            },
-          )
+          Expanded(
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(6.0),
+                  bottomRight: Radius.circular(6.0),
+                ),
+              ),
+              child: Text('确定'),
+              onPressed: () {
+                Log.d('确定');
+              },
+            ),
+          ),
         ],
       ),
     );
